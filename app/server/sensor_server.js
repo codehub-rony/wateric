f6f659e1cb9a8ed2358b690d80947f83ed697617
rony <rony@gisspecialisten.nl>
2021-06-28 22:45:33 +0200

    Added connection to db. First implementation of insert to table

--- app/server/sensor_server.js
+++ app/server/sensor_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const webSocket = require("ws");
+const db = require('./db/db.js')
 let http = require("http");
+
+require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +14,15 @@ s.on("connection", function(ws, req) {
   const ip = req.socket.remoteAddress;
   ws.on("message", function(message) {
     console.log(JSON.parse(message));
+    let data = JSON.parse(message)
+
+    const text = 'INSERT INTO soil_measurements(sensor_id, temp, moisture) VALUES($1, $2, $3) returning id'
+    const values = [1, int(data['temp']), data['moisture']]
+
+    db.query(text, values).then(res => {
+      console.log(res.rows[0])
+    })
+    .catch(e => console.error(e.stack))
     // s.clients.forEach(function(client) {
     //   if (client.readyState) {
     //     client.send("broadcast: " + message);
